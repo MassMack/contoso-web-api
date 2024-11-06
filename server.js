a16f5b6e7ed53e6effdc6152954e8bc2cde541ca
Norawee Mack <mack180553@gmail.com>
2024-11-06 08:48:22 +0000

    test3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 8000;
 const HOST = '0.0.0.0';
 const app = express();
 app.get('/',(req,res) => {
-    res.send('Hello World - 1.0.1');
+    res.send('Hello World - 1.0.2');
 });
 app.listen(PORT,HOST,() => {
     console.log(`Runnings1 On https://${HOST}:${PORT}`);
